024863d1c7ea6d022ebe9cea65f4e8438f103927
Nurby <70658958+Bruno-CTC@users.noreply.github.com>
2023-08-30 09:02:59 -0300

    Test commit

---
+++ app/src/main/java/com/example/socialfit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialfit
+
+import android.app.ActivityOptions
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
