da1c5a6685ffd2f7e3b5debbd6190fd119abc34c
Nurb <70658958+Bruno-CTC@users.noreply.github.com>
2023-10-10 12:25:14 -0300

    Update

--- app/src/main/java/com/example/socialfit/MainActivity.kt
+++ app/src/main/java/com/example/socialfit/MainActivity.kt
@@ -1,12 +1,44 @@
-package com.example.socialfit
+package com.example.socialfit;
 
+import android.annotation.SuppressLint
 import android.app.ActivityOptions
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestedOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        val button = findViewById<Button>(R.id.btnCadastrar);
+        button.setOnClickListener {
+            try {
+            } catch (e: Exception) {
+                AlertDialog.Builder(this)
+                    .setTitle("Erro")
+                    .setMessage(e.message)
+                    .setPositiveButton("OK", null)
+                    .show()
+            }
+        }
+
+        val button2 = findViewById<Button>(R.id.btnLoginCadastrar);
+        button2.setOnClickListener {
+            try {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+            } catch (e: Exception) {
+                AlertDialog.Builder(this)
+                    .setTitle("Erro")
+                    .setMessage(e.message)
+                    .setPositiveButton("OK", null)
+                    .show()
+            }
+        }
     }
 }
